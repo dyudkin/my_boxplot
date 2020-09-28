fbac7ddd1da018a64822e9114ad3305f445df42c
Daniel Yudkin <dyudkin@gmail.com>
2020-09-28 12:47:50 -0400

    new commit

--- my_boxplot demo.R
+++ my_boxplot demo.R
@@ -1,27 +1,20 @@
 
+clear()
 # Demo my_boxplot ---------------------------------------------------------
 require(tidyverse)
+source("my-boxplot.R")
+source("my-grouped-boxplot.R")
+
 
 # Figure 1 ----------------------------------------------------------------
 
 ggplot(iris, aes(x = Species, y = Sepal.Width)) +
   geom_boxplot()
 
-ggsave("Figure 1.png",
-       width = 5,
-       height = 5)
-
-
 # Figure 2 ----------------------------------------------------------------
 my_boxplot(iris, 
            x = "Species", 
            y = "Sepal.Width")
-
-ggsave("Figure 2.png",
-       width = 5,
-       height = 5)
-
-
 
 # Figure 3 ----------------------------------------------------------------
 my_boxplot(iris, 
@@ -29,28 +22,15 @@ my_boxplot(iris,
            y = "Sepal.Width",
            points = "None")
 
-ggsave("Figure 3a.png",
-       width = 5,
-       height = 5)
-
 my_boxplot(iris, 
            x = "Species", 
            y = "Sepal.Width",
            points = "jitter")
 
-ggsave("Figure 3b.png",
-       width = 5,
-       height = 5)
-
 my_boxplot(iris, 
            x = "Species", 
            y = "Sepal.Width",
            points = "count")
-
-ggsave("Figure 3c.png",
-       width = 5,
-       height = 5)
-
 
 # Figure 4 ----------------------------------------------------------------
 fills <- c("#838B8B", "#E0EEEE", "#79CDCD")
@@ -64,10 +44,6 @@ mtcars %>% mutate(cyl = factor(cyl)) %>%
              ylab = "Miles Per Gallon",
              fill = fills)
 
-ggsave("Figure 4.png",
-       width = 5,
-       height = 5)
-
 # Figure 5 ----------------------------------------------------------------
 my_boxplot(iris, 
            x = "Species", 
@@ -77,10 +53,6 @@ my_boxplot(iris,
   ggtitle("Sepal Width by Species") +
   theme(plot.title = element_text(hjust = 0.5))
 
-ggsave("Figure 5.png",
-       width = 5,
-       height = 5)
-
 # Grouped Boxplot ---------------------------------------------------------
 
 ToothGrowth %>% mutate(supp = factor(supp),
@@ -88,10 +60,6 @@ ToothGrowth %>% mutate(supp = factor(supp),
   my_grouped_boxplot(x = "dose",
                      y = "len",
                      group = "supp")
-ggsave("Figure 6.png",
-       width = 5,
-       height = 5)
-
 
 ToothGrowth %>% mutate(supp = factor(supp),
                        dose = factor(dose)) %>% 
@@ -100,17 +68,9 @@ ToothGrowth %>% mutate(supp = factor(supp),
                      group = "supp",
                      points = "count")
 
-ggsave("Figure 7a.png",
-       width = 5,
-       height = 5)
-
 ToothGrowth %>% mutate(supp = factor(supp),
                        dose = factor(dose)) %>% 
   my_grouped_boxplot(x = "dose",
                      y = "len",
                      group = "supp",
                      points = "jitter")
-
-ggsave("Figure 7b.png",
-       width = 5,
-       height = 5)
